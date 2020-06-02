6a49d9c500da377e1f63c72f50268942308d7404
kwsk-pro <maasa@kwsk.pro>
2020-06-02 11:58:30 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 知識ゼロからはじめる、Dockerコンテナ入門レッスン
+
+このGitレポジトリは、**知識ゼロからはじめる、Dockerコンテナ入門レッスン**で使用する教材です。
+[サンプル](https://kwsk-pro.github.io/my-first-docker/)のようなサイトをご自身のPCの中で、Dockerを使って動かします。
+テキストは、[こちら](https://github.com/kwsk-pro/my-first-docker/wiki/)です。
+
+- 参加方法 -> [StreetAcademy](https://www.street-academy.com/myclass/85090?conversion_name=direct_message&tracking_code=8af3373ce1f8ef05645e01682ea3a85d)
---
+++ php/source/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello World by PHP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet">
+  <link href="https://kwsk.pro/assets/streetacademy/85090/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="main">
+    <div class="text-wrap">
+      <h1>
+<?php
+  echo 'Hello world!' . '<br>';
+  echo 'Today is ' . date("Y-m-d") . ' UTC';
+?>
+      </h1>
+    </div>
+  </div>
+  <div class="footer">
+    <p>知識ゼロからはじめる、Dockerコンテナ入門講座</p>
+    <p>
+      Powered by <a href="https://kwsk.pro" target="_blank">kwsk.pro</a>
+    </p>
+  </div>
+</body>
+</html>
